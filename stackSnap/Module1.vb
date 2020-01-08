415a1780af19517e8d7c7650a4f0bf766288e335
Ryan <17U-RIrving@utccambridge.co.uk>
2020-01-08 11:13:20 +0000

    Add project files.

---
+++ stackSnap/Module1.vb
@@ -0,0 +1,58 @@
+﻿Module Module1
+
+    Structure card
+
+        Private FaceUp As Boolean
+
+
+    End Structure
+
+    Sub Main()
+
+        Dim deck(52) As Array
+
+        Dim placedStk As Stack = New Stack()
+
+        Console.WriteLine("oiufahpianoigajpogj")
+        Console.ReadLine()
+
+    End Sub
+
+    Function deckCreate()
+
+        Dim deck(52) As Array
+
+        Return deck
+
+    End Function
+
+    Sub shuffle()
+
+
+
+    End Sub
+
+
+    Sub handOut()
+
+        Dim playerDeck As Queue = New Queue()
+        Dim computerDeck As Queue = New Queue()
+
+
+
+    End Sub
+
+    Sub placeCards()
+
+
+
+    End Sub
+
+    Function snap()
+
+
+
+    End Function
+
+End Module
+
