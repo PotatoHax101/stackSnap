bfc6ad9ba9ddee618a96e73e53ccab5c79c37f18
Ryan <17U-RIrving@utccambridge.co.uk>
2020-01-15 12:21:41 +0000

    Started card placement and snap state

--- stackSnap/Module1.vb
+++ stackSnap/Module1.vb
@@ -7,6 +7,8 @@
 
         Dim shuffleDeck() As Integer = deckCreate(cards)
 
+        handOut(shuffleDeck)
+
         Console.ReadLine()
 
     End Sub
@@ -94,17 +96,69 @@
         Dim player1Deck As Stack(Of Integer) = New Stack(Of Integer)
         Dim player2Deck As Stack(Of Integer) = New Stack(Of Integer)
 
+        For i = 0 To 50 Step 2
 
+            player1Deck.Push(shuffleDeck(i))
+            player2Deck.Push(shuffleDeck(i + 1))
+
+        Next
+
+        placeCards(player1Deck, player2Deck)
 
     End Sub
 
-    Sub placeCards()
+    Sub placeCards(ByRef player1Deck As Stack(Of Integer), ByRef player2Deck As Stack(Of Integer))
 
         Dim cardDeposit As Stack(Of Integer) = New Stack(Of Integer)
 
+        cardDeposit.Push(player1Deck.Pop)
+        Dim previousPlacement As Integer = cardDeposit.Peek
+
+        Do
+
+            PlacementAction(player2Deck, player1Deck, previousPlacement, cardDeposit)
+            PlacementAction(player1Deck, player2Deck, previousPlacement, cardDeposit)
+
+        Loop While (player1Deck.Count > 0 And player2Deck.Count > 0) Or (player1Deck.Count > 0 And player2Deck.Count <= 0) Or (player1Deck.Count <= 0 And player2Deck.Count > 0)
+
     End Sub
 
-    Function snap()
+    Sub PlacementAction(ByRef firstDeck As Stack(Of Integer), ByRef secondDeck As Stack(Of Integer), ByRef previousPlacement As Integer, ByRef cardDeposit As Stack(Of Integer))
+
+        Dim chosenCard As Integer = firstDeck.Peek
+        cardDeposit.Push(firstDeck.Pop)
+
+        Dim CardCheckBool As Boolean = CardCheck(chosenCard, previousPlacement)
+
+        previousPlacement = chosenCard
+
+        If CardCheckBool = True Then
+
+            snapState(True, firstDeck, secondDeck)
+
+        Else
+
+            snapState(False, firstDeck, secondDeck)
+
+        End If
+
+    End Sub
+
+    Function CardCheck(ByVal chosenCard As Integer, ByVal previousCard As Integer)
+
+        If chosenCard = previousCard Then
+
+            Return True
+
+        Else
+
+            Return False
+
+        End If
+
+    End Function
+
+    Function snapState(ByVal bool As Boolean, ByRef player1Deck As Stack(Of Integer), ByRef player2Deck As Stack(Of Integer))
 
         Dim Deposit As Stack(Of Integer) = New Stack(Of Integer)
 
